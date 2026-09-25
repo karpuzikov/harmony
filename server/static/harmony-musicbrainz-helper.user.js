33921b8a691d725527dd50928c6538491d1ff021
karpuzikov <karpuzikovanton@gmail.com>
2026-09-25 07:29:46 +0300

    Run one-click helper after document load

--- server/static/harmony-musicbrainz-helper.user.js
+++ server/static/harmony-musicbrainz-helper.user.js
@@ -9,7 +9,7 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_openInTab
-// @run-at       document-start
+// @run-at       document-idle
 // ==/UserScript==
 
 (() => {
